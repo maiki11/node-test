bf80c858ff544433fa068fc08bec24fdb91116f5
maiki11 <miguelreina@gmail.com>
2023-03-22 23:59:27 -0700

    update

--- index.js
+++ index.js
@@ -4,4 +4,8 @@ app.get('/', (req, res) => {
   res.send('Hello, My jenkins World!\n');
 });
 
+app.get('/newEndpoint', (req, res) => {
+  res.send('My new endpoint!\n');
+});
+
 app.listen(3000, '0.0.0.0');
