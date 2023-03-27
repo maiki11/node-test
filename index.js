31f45b9052a1858e3038464565877615c76acf5d
maiki11 <miguelreina@gmail.com>
2023-03-26 23:32:31 -0700

    update

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const app = require('express')();
-
+const http = require('http');
+const host = '127.0.0.1';
+const port = 3000;
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Primer servidor con Node.Js');
+});
 app.get('/', (req, res) => {
   res.send('Hello this changes are uploaded on the instance!\n');
 });
@@ -8,4 +15,7 @@ app.get('/newEndpoint', (req, res) => {
   res.send('My new endpoint...!\n');
 });
 
+server.listen(port, host, () => {
+  console.log(`Servidor corriendo en http://${host}:${port}`);
+});
 app.listen(3000, 'localhost');
