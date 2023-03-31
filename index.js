a1938824d312c097518a5cddd0fe7a92300a09ce
maiki11 <miguelreina@gmail.com>
2023-03-31 01:21:24 -0700

    change part 3

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const app = require('express')();
 
 app.get('/', (req, res) => {
-  res.send('Hello this changes are uploaded on the instance part 2!\n');
+  res.send('Hello this changes are uploaded on the instance part 3!\n');
 });
 
 app.get('/newEndpoint', (req, res) => {
