3a5fa2c7f2bb2d490b7591240493afac0098da14
maiki11 <miguelreina@gmail.com>
2023-04-09 00:32:09 -0700

    port 80

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const app = require('express')();
 
 app.get('/', (req, res) => {
-  res.send('Hello this changes are uploaded on the instance part 4!\n');
+  res.send('Hello this changes are uploaded on the instance part 5!\n');
 });
 
 app.get('/newEndpoint', (req, res) => {
   res.send('My new endpoint!\n');
 });
 
-app.listen(3000, '0.0.0.0');
+app.listen(80, '0.0.0.0');
